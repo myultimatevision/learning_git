236a2dec26a0a6a305ae193deeb4986d6b77c292
Step5 <step5@Step5s-MacBook-Pro.local>
2019-11-18 18:15:04 +0530

    sum of two integers

--- test/testsum.js
+++ test/testsum.js
@@ -14,4 +14,9 @@ describe('sum',function(){
     assert.strictEqual(sum(-3,5),2);
   });
 
+  it('should add when either one or two numbers are zero',function() {
+    assert.strictEqual(sum(0,5),5);
+    assert.strictEqual(sum(-5,0),-5);
+    assert.strictEqual(sum(0,0),0);
+  });
 });
