e1606b1a08f3428b1369ae9a77ca54686e440b67
Step5 <step5@Step5s-MacBook-Pro.local>
2019-11-18 18:01:13 +0530

    sum of two negative or positive integers

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
 const sum = require('./src/sum').sum;
 
 const main = function() {
-  result =  sum(2,3);
+  number1 = +process.argv[2];
+  number2 = +process.argv[3];
+  result =  sum(number1,number2);
   console.log(result);
 };
 
--- test/testsum.js
+++ test/testsum.js
@@ -2,7 +2,12 @@ const assert = require('assert');
 const sum = require('../src/sum').sum;
 
 describe('sum',function(){
-it('should add two positive integers',function() {
-assert.strictEqual(sum(1,2),3);
-})
-})
+  it('should add two positive integers',function() {
+    assert.strictEqual(sum(1,2),3);
+  });
+
+  it('should add two negative integers',function() {
+    assert.strictEqual(sum(-1,-2),-3);
+  });
+
+});
